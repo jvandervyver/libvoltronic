edc138c152363ba330e1cebdfa576afd1fbd548b
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2019-03-22 00:49:12 +0200

    Minor cleanup

--- src/main.c
+++ src/main.c
@@ -1,9 +1,6 @@
-#include "voltronic_crc.h"
-#include "voltronic_dev.h"
 #include "voltronic_dev_serial.h"
 #include "voltronic_dev_usb.h"
 #include "voltronic_interface.h"
-#include "time_util.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -15,13 +12,12 @@ int main() {
   //voltronic_dev_t dev = voltronic_serial_create("/dev/tty.usbserial", 2400, DATA_BITS_EIGHT, STOP_BITS_ONE, SERIAL_PARITY_NONE);
   voltronic_dev_t dev = voltronic_usb_create(0x0665, 0x5161, 0);
   if (dev == 0) {
-    printf("Could not open /dev/usbserial\n");
+    printf("Could not open device\n");
     return 1;
   }
 
   char buffer[128];
   memset(buffer, 0, sizeof(buffer));
-  char* buffer_ptr = buffer;
   int result = voltronic_execute_command(dev, command, strlen(command), buffer, sizeof(buffer), 1000);
   if (result > 0) {
     printf("Success on command %s, got %s\n", command, buffer);
