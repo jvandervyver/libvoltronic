41894bc6604a4743fda7dd3bf1e08831e430e832
Johan van der Vyver <9843081+jvandervyver@users.noreply.github.com>
2020-03-27 12:17:29 +0200

    Update time_util.h
    Only allow uint64_t

--- include/time_util.h
+++ include/time_util.h
@@ -8,11 +8,7 @@
   #else
     #include <stdint.h>
 
-    #ifdef UINT64_MAX
-      typedef uint64_t millisecond_timestamp_t;
-    #elif UINT32_MAX
-      typedef uint32_t millisecond_timestamp_t;
-    #endif
+    typedef uint64_t millisecond_timestamp_t;
   #endif
 
   millisecond_timestamp_t get_millisecond_timestamp(void);
