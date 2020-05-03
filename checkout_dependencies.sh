fd283b5b8c6673251f327aeb42d14251673093a9
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-05-03 15:10:49 +0200

    Don't checkout latest anymore

--- checkout_dependencies.sh
+++ checkout_dependencies.sh
@@ -3,12 +3,9 @@
 git submodule init
 git submodule update
 cd lib/hidapi/
-git checkout master
 git pull
 cd ../libserialport/
-git checkout master
 git pull
 cd ../libusb/
-git checkout master
 git pull
 cd ../..
